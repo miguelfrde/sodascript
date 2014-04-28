775832687f674b4c64636c0d465ce9d225df018b
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-04-28 08:30:08 -0500

    Add mapping from operation to our JS stdlib functions.

--- lib/sodascript/semantic/expression.rb
+++ lib/sodascript/semantic/expression.rb
@@ -3,6 +3,25 @@ require 'set'
 module Sodascript
   class Expression
     attr_reader :variables
+    OP_TO_CODE = {
+      '+' => 'add',
+      '-' => 'sub',
+      '*' => 'prod',
+      '/' => 'div',
+      '%' => 'mod',
+      '**' => 'pow',
+      '<<' => 'shiftL',
+      '>>' => 'shiftR',
+      '^' => 'xor',
+      '&' => 'bitwiseAnd',
+      '|' => 'bitwiseOr'
+    }
+
+    OP_TO_CODE_SINGLE = {
+      '~s' => 'not',
+      '-s' => 'negative',
+      '+s' => 'positive'
+    }
 
     def initialize(variable_list, string)
       @variables = Set.new(variable_list)
@@ -10,7 +29,13 @@ module Sodascript
     end
 
     def self.create(a, op, b)
-      Expression.new(a.variables + b.variables, "#{a}#{op}#{b}")
+      fun = OP_TO_CODE[op]
+      fun_single = OP_TO_CODE_SINGLE[op]
+      bvars = (b && b.variables) || Set.new
+      variables = a.variables + bvars
+      (fun && Expression.new(variables, "#{fun}(#{a}, #{b})")) ||
+        (fun_single && Expression.new(variables, "#{fun_single}(#{a})")) ||
+        Expression.new(variables, "#{a} #{op} #{b}")
     end
 
     def to_s
