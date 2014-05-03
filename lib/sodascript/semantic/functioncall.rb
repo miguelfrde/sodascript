c9aedd1f1264334c2fa32982f0ef21981b6c1363
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-05-03 17:55:30 -0500

    Support

--- lib/sodascript/semantic/functioncall.rb
+++ lib/sodascript/semantic/functioncall.rb
@@ -4,21 +4,22 @@ module Sodascript
   class FunctionCall
     attr_reader :name, :arguments, :variables
 
-    def initialize(name, args)
+    def initialize(name, args, new_op = false)
       @name = name
       @arguments = args
       @string = "#{@name}(#{@arguments.join(', ')})"
+      @string = "new #{@string}" if new_op
       @variables = args.inject(Set.new) { |vars, expr| vars + expr.variables }
       @variables << name
     end
 
-    def self.create(name_var, args = [])
+    def self.create(name_var, args = [], new_op = false)
       if name_var.to_s.include?(".")
         variables = args.inject(Set.new) { |vars, expr| vars + expr.variables }
         string = "#{name_var}(#{args.join(', ')})"
         Variable.new(name_var.name, string, variables)
       else
-        FunctionCall.new(name_var.name, args)
+        FunctionCall.new(name_var.name, args, new_op)
       end
     end
 
