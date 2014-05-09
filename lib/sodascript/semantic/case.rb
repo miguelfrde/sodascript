99b7ff933abf0a83a9e94d12065c6d179715e193
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-05-08 20:18:59 -0500

    Fix Case statement, allow expressions

--- lib/sodascript/semantic/case.rb
+++ lib/sodascript/semantic/case.rb
@@ -1,16 +1,16 @@
 module Sodascript
   class Case
-    attr_reader :var, :whens, :default_block
+    attr_reader :expr, :whens, :default_block
 
-    def initialize(var, whens, default_block)
-      @var = var
+    def initialize(expr, whens, default_block)
+      @expr = expr
       @whens = whens
       @default_block = default_block
     end
 
     def to_s
       indent = Indentation.get
-      str = "#{indent}switch (#{@var}) {\n"
+      str = "#{indent}switch (#{@expr}) {\n"
       str << "#{@whens.map(&:to_s).join("\n")}\n"
       str << "#{indent}default:\n"
       Indentation.indent { str << "#{@default_block}\n" }
