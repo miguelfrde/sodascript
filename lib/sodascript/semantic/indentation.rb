bddec87c6b3861739e8df4fd0877fe401f68c9d2
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-05-13 23:56:21 -0500

    Add indentation documentation

--- lib/sodascript/semantic/indentation.rb
+++ lib/sodascript/semantic/indentation.rb
@@ -1,29 +1,57 @@
 module Sodascript
+
+  ##
+  # This module is used to indent the compiled javascript code, so that it's
+  # easier to read and debug.
+
   module Indentation
+
+    # Number of indentation spaces that will be printed
     @size = 0
+
+    # Indentation symbol(s)
     INDENTATION = '  '
+
+    ##
+    # Increment indentation by one
 
     def self.increment
       @size += 1
     end
 
+    ##
+    # Decrement indentation by 1
+
     def self.decrement
       @size -= 1
     end
 
+    ##
+    # Get indentation string
+
     def self.get
       INDENTATION * @size
     end
+
+    ##
+    # Increment and return indentation
 
     def self.increment_and_get
       increment_indent
       indentation
     end
 
+    ## Decrement and return indentation
+
     def self.decrement_and_get
       decrement_indent
       indentation
     end
+
+    ##
+    # This method is useful because it automatically increments the indentation
+    # before evaluating a block and automatically decrements it after the block
+    # has been evaluated.
 
     def self.indent(&block)
       increment
@@ -31,6 +59,10 @@ module Sodascript
       decrement
       result
     end
+
+    ##
+    # Return total indentation size. Useful when INDENTATION consists of more
+    # than one symbol
 
     def self.size
       @size * INDENTATION.size
